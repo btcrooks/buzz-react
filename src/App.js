1b7e89e9d835ee508121359a4c0a1a06105d280a
Brandon Crooks <brandon@brandons-mbp.home>
2019-04-07 00:53:13 -0400

    Its kinda working, i guess

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar'
+import LetterButtonMatrix from './components/LetterButtonMatrix.js'
+import Dictionary from './dictionary_2.json'
+console.log(Dictionary);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
         <NavBar></NavBar>
+        <LetterButtonMatrix
+          keyLetter={Dictionary.keyLetter}
+          letter={Dictionary.letters.split('')}
+        ></LetterButtonMatrix>
       </div>
     );
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
-        Hello from component!
+        <AppBar style={{ background: '#fff' }} position="static">
+          <Toolbar>
+            <Typography variant="body1" color="textSecondary">
+              Welcom to the BUZZ app!
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     )
   }
 }
+
